b12df566ef5511d7df9925fa8c6d858770b7f0b1
pengfei.li <18021055980@163.com>
2019-11-19 19:38:56 +0800

    基础常量

---
+++ src/main/java/com/hand/constant/BaseConstant.java
@@ -0,0 +1,46 @@
+package com.hand.constant;
+
+/**
+ * @author lei.shi06@hand-china.com
+ * @date 2019/1/24 16:20
+ **/
+
+public class BaseConstant {
+    // 字符编码
+    public static final String CHARSET_NAME = "UTF-8";
+    // webdriver相关变量
+    public static final String CHROME_DRIVER_NAME = "webdriver.chrome.driver";
+    public static final String FIREFOX_DRIVER_NAME = "webdriver.gecko.driver";
+    public static final String EDGE_DRIVER_NAME = "webdriver.edge.driver";
+    public static final String IE_DRIVER_NAME = "webdriver.ie.driver";
+    // 时间变量
+    public static final double TEN_OF_ONE = 0.1;
+    public static final int ONE_HUNDRED = 100;
+    public static final int FIVE_HUNDRED = 500;
+    public static final int ONE_THOUSAND = 1000;
+    public static final int TWO_THOUSANG = 2000;
+    public static final int THREE_THOUSANG = 3000;
+    public static final int FOUR_THOUSANG = 4000;
+    public static final int FIVE_THOUSANG = 5000;
+    public static final int SIX_THOUSANG = 6000;
+    public static final int SEVEN_THOUSANG = 7000;
+    public static final int EIGHT_THOUSANG = 8000;
+    public static final int NINE_THOUSANG = 9000;
+    public static final int TEN_THOUSANG = 10000;
+    public static final int ONE = 1;
+    public static final int TWO = 2;
+    public static final int THREE = 3;
+    public static final int FOUR = 4;
+    public static final int FIVE = 5;
+    public static final int SIX = 6;
+    public static final int ESEVEN = 7;
+    public static final int EIGHT = 8;
+    public static final int NINE = 9;
+    public static final int TEN = 10;
+    public static final int TWENTY = 20;
+    public static final int THIRTY = 30;
+    public static final int FORTY = 40;
+    // 默认字段
+    public static final String DEFAULT_VALUE = "0";
+    public static final String ONE_STR = "0";
+}
