da2c07b9d05da4a3648714e683b43d2cadacb191
pengfei.li <18021055980@163.com>
2019-11-29 11:40:38 +0800

    登录页面

---
+++ src/test/java/com/test/pc/page/LoginPage.java
@@ -0,0 +1,15 @@
+package com.test.pc.page;
+
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * 平台登录页面
+ *
+ * @author：关河九州
+ * @date：2019/11/21 19:36
+ * @version：1.0
+ */
+
+@Slf4j
+public class LoginPage extends PageCommon {
+}
